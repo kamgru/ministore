b65e3cd13193a32d9d22985f3890bb2b9235ab3d
kamgru <kamil.grudzien@hotmail.com>
2018-03-03 21:03:18 +0100

    Add project files.

---
+++ MiniStore.Domain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniStore.Domain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
